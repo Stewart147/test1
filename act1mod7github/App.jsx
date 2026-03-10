6c2b77ab067f42c2419b083c3f0648d20afb9f8f
Stewart147 <stewartsmart1@gmail.com>
2026-03-10 21:38:53 +0000

    Add files via upload

---
+++ act1mod7github/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import  Header  from "./components/Header"
+import  UserInput  from "./components/UserInput";
+import './index.css';
+
+//import UserInput from './components/UserInput';
+
+
+
+function App() {
+
+  return (
+
+    <>
+     
+      <Header />
+      <UserInput />
+    </>
+
+  );
+
+}
+
+export default App;
