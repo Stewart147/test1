a8c95e9efa0750172e49a79950b6e39f6aba9e41
Stewart147 <stewartsmart1@gmail.com>
2026-03-11 22:25:39 +0000

    Add files via upload

---
+++ act2mod7github/App.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import Header from './components/Header';
+import UserInput from './components/UserInput';
+import OutputData from './components/OutputData';
+
+function App() {
+
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10
+  });
+
+  const handleInputChange = (inputIdentifier, newValue) => {
+    setUserInput(prevInput => ({
+      ...prevInput,
+      [inputIdentifier]: +newValue
+    }));
+  };
+
+  return (
+    
+    <>
+      <Header />
+      <UserInput userInput={userInput} onInputChange={handleInputChange} />
+      <OutputData inputValue={userInput} />
+    </>
+  );
+
+}
+
+export default App;
---
+++ act2mod7github/components/OutputData.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { calculateInvestmentResults }  from '../util/investments.js';
+
+
+const OutputData = ({inputValue}) => {
+    // component logic will go here
+
+const resultData = calculateInvestmentResults({
+     initialInvestment: +inputValue.initialInvestment,
+     annualInvestment: +inputValue.annualInvestment,
+     expectedReturn: +inputValue.expectedReturn,
+     duration: +inputValue.duration
+});
+
+  return (
+
+    <div>
+
+        {/* component logic will go here */}
+
+        <table>
+    <thead>
+      <tr>
+        <th>Year</th>
+        <th>Investment Value</th>
+        <th>Interest (Year)</th>
+        <th>Total Interest</th>
+        <th>Invested Capital</th>
+      </tr>
+    </thead>
+    <tbody>
+      {resultData.map((yearData, index) => (
+        <tr key={index}>
+          <td>{yearData.year}</td>
+          <td>{yearData.investmentValue.toFixed(2)}</td>
+          <td>{yearData.interest.toFixed(2)}</td>
+          <td>{yearData.totalInterest.toFixed(2)}</td>
+          <td>{yearData.investedCapital.toFixed(2)}</td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+
+    </div>
+
+  );
+
+};
+
+export default OutputData;
